674c63d1627289f89421bbc77c26efe889e63f8e
liaoyi <2417276459@qq.com>
2023-05-30 19:58:26 +0800

    格式: 🎨 style.

--- src/components/Home/Banner.vue
+++ src/components/Home/Banner.vue
@@ -6,7 +6,7 @@
 				<div class="divBlock70"></div>
 			</div>
 			<div class="container mx-auto md:px-4 px-6 flex pt-20 flex-col justify-center items-center gap-3">
-				<div class="text-block text-5xl">{{ $t("home.cards.title") }}</div>
+				<div class="text-block text-5xl" style="text-align: center">{{ $t("home.cards.title") }}</div>
 				<CuttingLine class="mt-2" />
 			</div>
 		</div>
